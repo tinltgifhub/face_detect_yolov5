8d4adaacffba0739946994efce73e41f318cb9da
tinltgifhub <82744081+tinltgifhub@users.noreply.github.com>
2022-12-21 15:59:25 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -205,6 +205,8 @@ def run(
 
         # Print time (inference-only)
         LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+        print(s)
+        print(dt)
 
     # Print results
     t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
